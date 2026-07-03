5c1ad01b93a8c11e79b3d344b834c0579d12f6af
Olajuwaan-Larenzo Lewis <153092963+Elovvaan@users.noreply.github.com>
2026-07-02 18:04:49 -0600

    Wire media role profile form

--- web/src/app/media/page.tsx
+++ web/src/app/media/page.tsx
@@ -1,5 +1,6 @@
+import { RoleProfileForm } from "@/components/role-profile-form";
 import { RoleWorkspace } from "@/components/role-workspace";
 
 export default function MediaWorkspacePage() {
-  return <RoleWorkspace role="media" />;
+  return <div><RoleWorkspace role="media" /><div className="bg-[#061331] px-4 pb-10 lg:pl-[292px]"><div className="mx-auto max-w-[1680px]"><RoleProfileForm role="media" /></div></div></div>;
 }
