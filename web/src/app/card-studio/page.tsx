b039a80f6e2521dfce5145c701a78f98f84965d1
Olajuwaan-Larenzo Lewis <153092963+Elovvaan@users.noreply.github.com>
2026-07-11 20:56:21 -0600

    Add MYD1 card studio page

---
+++ web/src/app/card-studio/page.tsx
@@ -0,0 +1,10 @@
+import { AppShell } from "@/components/app-shell";
+import { CardStudio } from "@/components/card-studio";
+
+export default function CardStudioPage() {
+  return (
+    <AppShell>
+      <CardStudio />
+    </AppShell>
+  );
+}
